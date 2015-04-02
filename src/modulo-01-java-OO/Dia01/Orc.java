1e3e7c267ba4c88da1b0eaedf738bdcc8b9cd82c
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2015-04-02 17:20:32 -0300

    classe Org

---
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -0,0 +1,33 @@
+
+/**
+ * Escreva a descrição da classe Orc aqui.
+ * 
+ * @author Rodrigo Dagnese
+ * @version (número de versão ou data)
+ */
+public class Orc
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String nome;
+    private int life;
+    
+    {
+        life = 110;
+    }
+
+    /**
+     * Construtor para objetos da classe Orc
+     */
+    public Orc(String nome)
+    {
+        this.nome = nome;
+    }
+
+    /**
+     * Orc recebe flechada e perde 10 pontos de vida
+     */
+    public void recebeFlechada()
+    {
+        this.life -= 10;
+    }
+}
