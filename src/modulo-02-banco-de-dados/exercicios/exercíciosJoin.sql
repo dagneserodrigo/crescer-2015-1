b4de1ca9e2367f1da93ae2c087e9310b63017e1e
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2015-04-24 16:16:25 -0300

    feat(BancoDeDados) exercícios sobre joins finalizados

--- src/modulo-02-banco-de-dados/exercicios/exercíciosJoin.sql
+++ src/modulo-02-banco-de-dados/exercicios/exercíciosJoin.sql
@@ -11,23 +11,26 @@ from Associado a
 left join Cidade c on a.IDCidade = c.IDCidade; 
 
 --3
-Select Nome,
-	   UF
+Select UF,
+	   Count(1) As TotalDeCidades
 from Cidade c
 Where not exists (Select IDCidade
 				  from Associado a
-				  Where a.IDCidade = c.IDCidade);
+				  Where a.IDCidade = c.IDCidade)
+group by UF;
 	   	  
 --4
+--create view vw_Cidade_Regiao as
 (Select a.Nome, 
 		c.IDCidade,
-		(Case When UF = 'RS' then '***'
-			  When UF = 'SC' then '***'
-			  When UF = 'PR' then '***'
+		(Case When c.UF in ('RS', 'SC', 'PR') then '***'
 			  Else null
-			  End) UF
+			  End) RegiaoSul
 from Associado a
 inner join Cidade c on c.IDCidade = a.IDCidade);
+
+--utilizando view
+select * from vw_Cidade_Regiao
 
 --5
 Select e.NomeEmpregado,
@@ -49,25 +52,37 @@ from Empregado;
 
 Update Empregado
 Set Salario = (Salario + (Salario*0.145))
-Where IDEmpregado in (Select e.IDEmpregado
-					  from Empregado e
-                      inner join Departamento d on e.IDDepartamento = d.IDDepartamento
+Where IDDepartamento in (Select IDDepartamento
+					  from Departamento d
                       Where d.Localizacao = 'SAO PAULO');
-
 rollback
 commit
 
 --7
-
+Select SUM(EmpregadoCopy.Salario) as TotalSalarioAnterior,
+	   SUM(Empregado.Salario) as TotalSalarioAtual,
+       (SUM(Empregado.Salario) - SUM(EmpregadoCopy.Salario)) as DiferencaSalarial
+from Empregado
+join EmpregadoCopy on Empregado.IDEmpregado = EmpregadoCopy.IDEmpregado;
 
 --8
 Select d.NomeDepartamento
 from Departamento d
-Where EXISTS (Select MAX(e.IDDepartamento)
-			  From Empregado e
-			  where e.IDDepartamento = d.IDDepartamento);
+Where d.IDDepartamento IN (Select top 1 e.IDDepartamento
+						   From EmpregadoCopy e
+						   group by e.IDDepartamento
+						   order by 1 desc);
 
 --9
+Select a.Nome,
+	   c.Nome
+from Associado a
+inner join Cidade c on a.IDCidade = c.IDCidade
+Union
+Select e.NomeEmpregado,
+	   d.Localizacao
+from Empregado e
+inner join Departamento d on e.IDDepartamento = d.IDDepartamento;
 
 --10
 Select c.Nome
