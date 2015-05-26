b263229f40e308903906440e8b073807eed2a99a
dagneserodrigo <dagnese.rodrigo@gmail.com>
2015-05-25 23:57:40 -0300

    refactor(FilmeDao) ajustado para conexao com o banco. Métodos de inserir, buscar todos e buscar filme pelo nome

--- src/modulo-04/filmator-web/src/main/java/filmator/dao/FilmeDao.java
+++ src/modulo-04/filmator-web/src/main/java/filmator/dao/FilmeDao.java
@@ -1,27 +1,52 @@
 package filmator.dao;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import filmator.model.Filme;
+import javax.inject.Inject;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import filmator.model.Filme;
+import filmator.model.Genero;
+
+@Component
 public class FilmeDao {
-	private List<Filme> filmes = new ArrayList<>();
+
+	@Inject
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public void inserir(Filme filme){
+		jdbcTemplate.update("INSERT INTO Filme (Nome, Genero, Ano_lancamento, Sinopse, Imagem) VALUES (?, ?, ?, ?, ?)",
+						filme.getNome(), filme.getGenero().name(), filme.getAnoLancamento(), filme.getSinopse(), filme.getImagem());
+	}
 
 	public List<Filme> buscaTodosFilmes(){
-		//Imagina que estes dados estao vindo do banco
-		return Arrays.asList(new Filme(),
-			new Filme(),
-			new Filme());
+		return jdbcTemplate.query("SELECT Nome, Genero, Ano_lancamento, Sinopse, Imagem FROM Filme", (ResultSet rs, int rowNum) -> {
+			Filme filme = new Filme();
+			filme.setNome(rs.getString("Nome"));
+			filme.setGenero(Genero.valueOf(rs.getString("Genero")));
+			filme.setAnoLancamento(rs.getInt("Ano_lancamento"));
+			filme.setSinopse(rs.getString("Sinopse"));
+			filme.setImagem(rs.getString("Imagem"));
+			return filme;
+		});	
 	}
 	
-	public void salvarFilme(Filme filme) {
-		filmes.add(filme);
-	}
-	
-	public List<Filme> consultaFilmes() {
-		return filmes;
+	public List<Filme> buscaFilmePeloNome(String nome) {
+		return jdbcTemplate.query("SELECT Nome, Genero, Ano_lancamento, Sinopse, Imagem FROM Filme WHERE Nome LIKE ? or Nome LIKE ?", new String[]{nome.toLowerCase() + "%", nome.toUpperCase() + "%"} , (ResultSet rs, int rowNum) -> {
+			Filme filme = new Filme();
+			filme.setNome(rs.getString("Nome"));
+			filme.setGenero(Genero.valueOf(rs.getString("Genero")));
+			filme.setAnoLancamento(rs.getInt("Ano_lancamento"));
+			filme.setSinopse(rs.getString("Sinopse"));
+			filme.setImagem(rs.getString("Imagem"));
+			return filme;
+		});
 	}
 }
  
