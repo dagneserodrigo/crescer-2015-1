9a1f857f2fba10942bea9fc1aa4eb0f4963d5060
dagneserodrigo <dagnese.rodrigo@gmail.com>
2015-05-25 23:56:31 -0300

    feat(FilmeController) busca de filme pelo nome

--- src/modulo-04/filmator-web/src/main/java/filmator/controller/FilmeController.java
+++ src/modulo-04/filmator-web/src/main/java/filmator/controller/FilmeController.java
@@ -1,9 +1,15 @@
 package filmator.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.logging.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import filmator.dao.FilmeDao;
 import filmator.model.Filme;
@@ -11,24 +17,32 @@ import filmator.model.Genero;
 
 @Controller
 public class FilmeController {
-	private FilmeDao dao = new FilmeDao();
+	
+	@Inject 
+	private FilmeDao filmeDao;
 
-	@RequestMapping(value = "/cadastrarFilme", method = RequestMethod.GET)
+	@RequestMapping(value = "/inserir", method = RequestMethod.GET)
 	public String cadastro(Model model){
 		model.addAttribute("generos", Genero.values());
  		return "filmeCadastro";
  	}
-
-	@RequestMapping(value = "/salvarFilme", method = RequestMethod.POST)
-	public String salvarFilme(Filme filme, Model model){
-		dao.salvarFilme(filme);
-		model.addAttribute("filmes", dao.consultaFilmes());
- 		return "filmeConsulta";
- 	}
+	
+	@RequestMapping(value = "/inserir", method = RequestMethod.POST)
+	public String inserir(Model model, Filme filme) {
+		filmeDao.inserir(filme);
+		model.addAttribute("filmes", filmeDao.buscaTodosFilmes());
+		return "filmeConsulta";
+	}
 	
 	@RequestMapping(value = "/consultarFilme", method = RequestMethod.GET)
 	public String consultarFilme(Model model) {
-		model.addAttribute("filmes", dao.consultaFilmes());
+		model.addAttribute("filmes", filmeDao.buscaTodosFilmes());
+		return "filmeConsulta";
+	}
+	
+	@RequestMapping(value = "/procurar", method = RequestMethod.GET)
+	public String procurar(Model model, @RequestParam String nome) {
+		model.addAttribute("filmes", filmeDao.buscaFilmePeloNome(nome));
 		return "filmeConsulta";
 	}
 }
