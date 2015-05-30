904f0c69372b5c64855758e0cd3ca881afa55699
dagneserodrigo <dagnese.rodrigo@gmail.com>
2015-05-29 23:39:56 -0300

    feat(HomeController) correção do problema de acentuação

--- src/modulo-04/filmator-web/src/main/java/filmator/controller/HomeController.java
+++ src/modulo-04/filmator-web/src/main/java/filmator/controller/HomeController.java
@@ -1,18 +1,41 @@
 package filmator.controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import filmator.model.Genero;
+import filmator.dao.AtorDao;
+import filmator.dao.BannerDao;
+import filmator.dao.FilmeDao;
+import filmator.dao.UsuarioDao;
 
 @Controller
 public class HomeController {
 
+	@Inject
+	private AtorDao atorDao;
 	
+	@Inject
+	private FilmeDao filmeDao;
+	
+	@Inject
+	private UsuarioDao usuarioDao;
+	
+	@Inject
+	private BannerDao bannerDao;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Model model, HttpSession session) {
+		if(usuarioDao.usuarioEstaLogado(session)) {
+			model.addAttribute("usuarioLogado", true);
+		}
+		model.addAttribute("banners", bannerDao.buscarBannersDestaques());
+		model.addAttribute("atores", atorDao.buscaAtoresDestaques());
+		model.addAttribute("filmes", filmeDao.buscaFilmesDestaques());
 		return "home";
 	}
 }
